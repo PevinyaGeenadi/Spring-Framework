ccaf3b2e7092211b6f9803ee928d976e8430144f
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-02-16 12:19:14 +0530

    update

--- src/main/java/lk/ijse/gdse/api/bean/Tharushi.java
+++ src/main/java/lk/ijse/gdse/api/bean/Tharushi.java
@@ -3,6 +3,6 @@ package lk.ijse.gdse.api.bean;
 public class Tharushi implements GoodGirl{
     @Override
     public void love(){
-
+          System.out.println("Tharushi love him so much");
     }
 }
