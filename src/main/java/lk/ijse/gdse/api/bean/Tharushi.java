f1eaf3975ed55a7561c0363e96f7a9c91250a2ee
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-02-16 14:48:13 +0530

    update

--- src/main/java/lk/ijse/gdse/api/bean/Tharushi.java
+++ src/main/java/lk/ijse/gdse/api/bean/Tharushi.java
@@ -1,5 +1,13 @@
 package lk.ijse.gdse.api.bean;
 
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+@Qualifier("Long Hair")
 public class Tharushi implements GoodGirl{
     @Override
     public void love(){
