eebd1bd5d9640beff15a57de40d48adc5317f0c3
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-02-16 16:38:31 +0530

    update

--- src/main/java/lk/ijse/gdse/api/AppConfig.java
+++ src/main/java/lk/ijse/gdse/api/AppConfig.java
@@ -3,22 +3,28 @@ package lk.ijse.gdse.api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
 
 @ComponentScan
 @Configuration
 public class AppConfig {
+    @Order(2)
     @Bean
-    public String myString1(){
-        return null;
+    public String mySring1() {
+        return "IJSE";
     }
 
+    @Order(3)
     @Bean
-    public String myString2(){
-        return null;
+    @Primary
+    public String myString2() {
+        return "ESOFT";
     }
 
+    @Order(1)
     @Bean
-    public String myString3(){
-        return null;
+    public String myString3() {
+        return "SLIT";
     }
 }
--- src/main/java/lk/ijse/gdse/api/bean/SpringBean.java
+++ src/main/java/lk/ijse/gdse/api/bean/SpringBean.java
@@ -1,10 +1,36 @@
 package lk.ijse.gdse.api.bean;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class SpringBean {
-    public SpringBean(String name){
-        System.out.println("SpringBean() + Constructor");
+    public SpringBean(@Qualifier("note")String[] nameList){
+        System.out.println("SpringBean() + constructor" + nameList);
+        System.out.println(Arrays.toString(nameList));
+
     }
+
+
+    /*public SpringBean(List<String> nameList) {
+        System.out.println("SpringBean() - constructor -" + nameList);
+        System.out.println(nameList);
+    }*/
+
+
+    /* public SpringBean(Set<String> nameList) {
+        System.out.println("SpringBean() - constructor -" + nameList);
+        System.out.println(nameList);
+    } */
+
+
+     /* public SpringBean(List<GoodGirl> GoodGirl) {
+        System.out.println("SpringBean() - constructor -" + GoodGirl);
+        System.out.println(GoodGirl);
+    }*/
 }
